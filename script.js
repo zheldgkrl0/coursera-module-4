49aa899f13f0ad00f5f549aa3915853388ab7165
zheldgkrl0 <159199237+zheldgkrl0@users.noreply.github.com>
2024-03-03 16:45:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
+(function () {
 
-var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
-
-
-for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
-	}
-	else{
-		console.log("Hello "+ names[i])
-	}
-}
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } 
+      else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    })();
